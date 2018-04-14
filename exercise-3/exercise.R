2a6f7d235f197ddf96972cbf3ae9cc3451c5c8b0
Leo Kwo <leoguo@MBP.local>
2018-04-13 21:39:14 -0700

    changed exercise 2

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,17 +2,17 @@
 
 # Load R's "USPersonalExpenditure" dataset using the `data()` function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data("USPersonalExpenditure")
 
 # The variable `USPersonalExpenditure` is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can pass the USPersonalExpenditure variable as an argument to the
 # `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+uspe <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
 
@@ -20,17 +20,23 @@
 ## Consider: why are they so strange? Think about whether you could use a number 
 ## like 1940 with dollar notation!
 
+
 # What are the row names of your dataframe?
 
 
 # Add a column "category" to your data frame that contains the rownames
-
+uspe$categoory <- rownames(uspe)
 
 # How much money was spent on personal care in 1940?
-
+rownames(uspe)
+colnames(uspe)
+pc_money_1940 <- uspe['X1940','personal care'] ...?
+pc_money_1940 <- uspe['Personal Care','X1940']
 
 # How much money was spent on Food and Tobacco in 1960?
-
+ft_money_1960 <- uspe['X1960']
+is.data.frame(ft_money_1960)
+ft_money_1960[,'food and tabacco']
 
 # What was the highest expenditure category in 1960?
 
